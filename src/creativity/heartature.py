fe972e8bf87cf0702c312206660c2f9c83cc55a9
Rohan Bari <rohanbari4@gmail.com>
2023-04-15 09:23:43 +0530

    Heart-shaped curvature on graph
    Signed-off-by: Rohan Bari <rohanbari4@gmail.com>

---
+++ src/creativity/heartature.py
@@ -0,0 +1,17 @@
+# Heart-shaped graph using mathematics
+
+import numpy as np
+import matplotlib.pyplot as mp
+
+t = np.linspace(0, 2*np.pi, 100)
+
+x = 16*np.sin(t)**3
+y = 13*np.cos(t) - 5*np.cos(2*t) - 2*np.cos(3*t) - np.cos(4*t)
+
+mp.plot(x, y, c=(1, 0.2, 0.5), lw=20)
+
+mp.title('To my mathematics:')
+mp.axis('equal')
+mp.axis('off')
+
+mp.show()
